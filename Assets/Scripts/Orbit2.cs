c23a0891d258970838a792e416492db42f48f456
Darian Springer <darianc.springer@gmail.com>
2015-03-23 11:58:57 -0400

    added rotating cameras

---
+++ Assets/Scripts/Orbit2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 camAngle = new Vector3 (20, -90, 0);
+		transform.RotateAround (GameObject.FindGameObjectWithTag ("Player").transform.position, camAngle, 20 * Time.deltaTime);
+	}
+}
